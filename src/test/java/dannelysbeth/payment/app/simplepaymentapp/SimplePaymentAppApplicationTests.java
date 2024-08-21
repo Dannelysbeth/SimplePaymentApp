536889d70637411fdecdd17c4a6419ddae0b92a1
Dannelysbeth <danielaf@onet.pl>
2024-08-21 23:21:42 +0200

    init project

---
+++ src/test/java/dannelysbeth/payment/app/simplepaymentapp/SimplePaymentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dannelysbeth.payment.app.simplepaymentapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplePaymentAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
