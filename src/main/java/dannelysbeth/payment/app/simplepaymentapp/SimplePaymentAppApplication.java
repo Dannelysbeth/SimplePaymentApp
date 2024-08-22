fd17c0fc9b8d89e2d0433189816fcfb34490d0ef
Dannelysbeth <danielaf@onet.pl>
2024-08-22 23:13:47 +0200

    added springdoc mapping

--- src/main/java/dannelysbeth/payment/app/simplepaymentapp/SimplePaymentAppApplication.java
+++ src/main/java/dannelysbeth/payment/app/simplepaymentapp/SimplePaymentAppApplication.java
@@ -3,7 +3,7 @@ package dannelysbeth.payment.app.simplepaymentapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"dannelysbeth.payment.app.simplepaymentapp.*", "org.springdoc"})
 public class SimplePaymentAppApplication {
 
     public static void main(String[] args) {
